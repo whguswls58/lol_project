95daf94580edaba03a0a8120d3d87dea045315b8
HyeonJin <whguswls58@naver.com>
2025-05-30 23:28:08 +0900

    불필요한 import 된 라이브러리 삭제

--- lolproject/src/main/java/com/example/demo/champion/controller/ChampionController.java
+++ lolproject/src/main/java/com/example/demo/champion/controller/ChampionController.java
@@ -1,28 +1,19 @@
 package com.example.demo.champion.controller;
 
 import java.io.FileReader;
-import java.io.InputStream;
 import java.io.Reader;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.champion.service.ChampionService;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 public class ChampionController {
